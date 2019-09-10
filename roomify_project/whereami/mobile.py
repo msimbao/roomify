62eff0c6a7f2b8605f661f9376c60c0ba35ae878
msimbao <32774483+msimbao@users.noreply.github.com>
2019-09-09 22:12:56 -0500

    Delete mobile.py

--- roomify_project/whereami/mobile.py
+++
@@ -1,6 +0,0 @@
-# # mobile
-# case = {}
-# for row in [data['activity']] + data['available']:
-#     key = row['SSID'] + " " + row['BSSID'] + " " + row.get('capabilities', '')
-#     value = row.get("RSSI", row.get("level", ''))
-#     case[key] = value
